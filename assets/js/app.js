a89ba0922c209de7b059c17ee69bd770654855b0
ok118com <admin@xmyy.com>
2024-07-15 09:22:24 +0800

    add link

--- assets/js/app.js
+++ assets/js/app.js
@@ -25,27 +25,27 @@ document.addEventListener('DOMContentLoaded', async () => {
     	  console.error(error);
         resultDiv.innerHTML += '，获取IP归属地失败！';
     }
-    lookupBtn.addEventListener('click', async () => {
-        const ipAddress = ipInput.value.trim();
-        const validIPRegex = /^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
-        if (validIPRegex.test(ipAddress)) {
-            loadingIndicator.classList.remove('hidden');
-            try {
-                const response = await fetch(`${proxyServer}/proxy/query?ip=${ipAddress}`);
-                const data = await response.text();
-                const parser = new DOMParser();
-                const doc = parser.parseFromString(data, 'text/html');
-                const addressDiv = doc.querySelector('tbody tr:first-child td:nth-child(3)');
-                const addressText = addressDiv ? addressDiv.textContent.trim() : '';
-                resultDiv.innerHTML = `您查询的IP: ${ipAddress}，其归属地为：${addressText}。`;
-                loadingIndicator.classList.add('hidden');
-            } catch (error) {
-                resultDiv.innerHTML = `您查询的IP: ${ipAddress}，获取IP归属地失败！`;
-                loadingIndicator.classList.add('hidden');
-            }
-        } else {
-            resultDiv.innerHTML = '您输入的IP地址格式不正确，请检查后重试。';
-            loadingIndicator.classList.add('hidden');
-        }
-    });
+    // lookupBtn.addEventListener('click', async () => {
+    //     const ipAddress = ipInput.value.trim();
+    //     const validIPRegex = /^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
+    //     if (validIPRegex.test(ipAddress)) {
+    //         loadingIndicator.classList.remove('hidden');
+    //         try {
+    //             const response = await fetch(`${proxyServer}/proxy/query?ip=${ipAddress}`);
+    //             const data = await response.text();
+    //             const parser = new DOMParser();
+    //             const doc = parser.parseFromString(data, 'text/html');
+    //             const addressDiv = doc.querySelector('tbody tr:first-child td:nth-child(3)');
+    //             const addressText = addressDiv ? addressDiv.textContent.trim() : '';
+    //             resultDiv.innerHTML = `您查询的IP: ${ipAddress}，其归属地为：${addressText}。`;
+    //             loadingIndicator.classList.add('hidden');
+    //         } catch (error) {
+    //             resultDiv.innerHTML = `您查询的IP: ${ipAddress}，获取IP归属地失败！`;
+    //             loadingIndicator.classList.add('hidden');
+    //         }
+    //     } else {
+    //         resultDiv.innerHTML = '您输入的IP地址格式不正确，请检查后重试。';
+    //         loadingIndicator.classList.add('hidden');
+    //     }
+    // });
 });
