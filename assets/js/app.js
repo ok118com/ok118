713d6c8bb7bffcbce01bef303bf6a061f538f9ef
ok118com <admin@xmyy.com>
2024-11-21 22:44:23 +0800

    modify link

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,12 +3,12 @@ document.addEventListener('DOMContentLoaded', async () => {
     const ipInput = document.getElementById('ipInput');
     const lookupBtn = document.getElementById('lookupBtn');
     const loadingIndicator = document.getElementById('loadingIndicator');
-    const proxyServer = "https://file.xyj321.com"
+    const proxyServer = "https://www.ip138.com"
     try {
         const response = await fetch('https://ipinfo.io/ip');
         const ip = await response.text();
         resultDiv.innerHTML = `您的当前IP是: ${ip}`;
-        const ipDetailsResponse = await fetch(`${proxyServer}/proxy/query?ip=${ip}`);
+        const ipDetailsResponse = await fetch(`${proxyServer}/iplookup.php?ip=${ip}&action=2`);
         if (!ipDetailsResponse.ok) {
             throw new Error('Failed to fetch IP details.');
         }
